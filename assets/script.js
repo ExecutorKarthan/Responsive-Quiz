ba73600b7bc705c91d5f7869f994edfb91025d42
Alex Messina <me@alexmessina.dev>
2023-10-18 22:32:03 -0500

    Decided on static JSON -Client may not have node.js which will fail the project -Added function to formate question to question box -Adjusted JavaScript to ensure question objects were made and passed

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,3 @@
-import data from "./questionBank.json" assert { type: "json"};
-
-class questionObj{
-    constructor(question, correctAnswer, options){
-        this. question = question;
-        this. correctAnswer = correctAnswer;
-        this.options = shuffleAnswers(options)
-    }
-}
-
-function shuffleAnswers(options){
-    event.stopPropagation()
-    for( var i = options.length-1; i > 0; i--){
-        var tempEnd = options[i]
-        var swapIndex = Math.floor(Math.random()*i)
-        options[i] = options[swapIndex]
-        options[swapIndex] = tempEnd
-    }
-}
-
-function prepQuestions(){
-    event.stopPropagation()
-    var questionData = data
-    for( var i = 1; i < Object.values(questionData).length+1; i++){
-        questionArray.push(new questionObj(questionData[i][0], questionData[i][1], questionData[i].slice(1)))
-    }
-}
-
 function changeBox(visibleBox){
     event.stopPropagation()
     var welcomePage = document.querySelector("#welcomePage");
@@ -45,24 +17,97 @@ function changeBox(visibleBox){
     }    
 }
 
-function runGame(){
-    event.stopPropagation()
-    prepQuestions()
-    changeBox("#questionBox")
-    var timerInterval = setInterval(function() {
-    timeVal--;
-    timeText.textContent = timeVal;
-    if(timeVal == 0){
-        clearInterval(timerInterval)
+function prepQuestions(questionArray){
+    for( var i = 1; i < Object.values(questionDatabase).length+1; i++){
+        questionArray.push(new questionObj(questionDatabase[i][0], 
+            questionDatabase[i][1], 
+            questionDatabase[i].slice(1)))
     }
-    }, 1000)
+    return questionArray
+}
+class questionObj{
+    constructor(question, correctAnswer, options){
+        this. question = question;
+        this. correctAnswer = correctAnswer;
+        this.options = shuffleAnswers(options)
+    }
+}
+
+function shuffleAnswers(options){
+    for( var i = options.length-1; i > 0; i--){
+        var tempEnd = options[i]
+        var swapIndex = Math.floor(Math.random()*i)
+        options[i] = options[swapIndex]
+        options[swapIndex] = tempEnd
+    }
+    return options
+}
+
+function formateQuestion(questionArray){
+    var selectedQuestion = questionArray[Math.floor(Math.random()*questionArray.length)]
+    document.querySelector("#question").textContent = selectedQuestion.question;
+    var responseLocations = document.getElementById("responses").children;
+    for(var i = 0; i < selectedQuestion.options.length; i++){
+        responseLocations[i].textContent = String(i+1) + ")  " + selectedQuestion.options[i];
+    }
 }
 
 function enterAnswer(){
 
 }
 
-var questionArray = []
+function runGame(){
+    changeBox("#questionBox");
+    formateQuestion(questionArray);
+    var timerInterval = setInterval(function() {
+    timeVal--;
+    timeText.textContent = timeVal;
+    enterAnswer();
+    if(timeVal == 0){
+        clearInterval(timerInterval)
+    }
+    }, 1000)
+}
+
+var questionDatabase = {
+    "1": [
+        "Which of the following will call the 3rd item of an array?",
+        "array[2]",
+        "array[3]", 
+        "array.get(3rdPlace)",
+        "tarray.pop()"
+    ],
+    "2": [
+        "A for loop is composed of",
+        "all of these are true",
+        "a variable declaration",
+        "a conditional to continue to run",
+        "an incrementer"
+    ],
+    "3": [
+        "If statements execute code",
+        "when their conditions evaluate as true",
+        "when their conditions evaluate as false",
+        "under all conditions",
+        "when conditions change from false to true"
+    ],
+    "4": [
+        "The variable \"age\" is best represented by",
+        "an int",
+        "a float",
+        "a string",
+        "an array"
+    ],
+    "5": [
+        "Functions can be created",
+        "by declaring the \"function\" keyword",
+        "by using \"def_functionName\"",
+        "only in classes in Javascript",
+        "None of these are true"
+    ]
+}
+var questionArray = [];
+prepQuestions(questionArray);
 var leaderText = document.querySelector("#leaderLink")
 var timeText = document.querySelector("#startTime")
 var timeVal = document.querySelector("#startTime").innerHTML
