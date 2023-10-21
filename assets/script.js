3794eb81f9c7597be77cabe21a8c2e397b0f9405
Alex Messina <me@alexmessina.dev>
2023-10-21 15:45:19 -0500

    Commented all code for documentation purposes

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//Create a function to change between boxes/screens
 function changeBox(visibleBox){
     var visibleBox = visibleBox.getAttribute("id")
     welcomePage.style.display="None";
@@ -17,7 +18,8 @@ function changeBox(visibleBox){
         quizCompleted.style.display="flex";    
     }
 }
-
+/*Create a function to prepare the questions for use. It will create a question array with question objects that hold the question, 
+the correct answer and a randomly generated order of responses*/
 function prepQuestions(questionArray){
     for( var i = 1; i < Object.values(questionDatabase).length+1; i++){
         questionArray.push(new questionObj(questionDatabase[i][0], 
@@ -26,6 +28,7 @@ function prepQuestions(questionArray){
     }
     return questionArray
 }
+/*Create a question class for easy access of the question data*/
 class questionObj{
     constructor(question, correctAnswer, options){
         this. question = question;
@@ -33,7 +36,8 @@ class questionObj{
         this.options = shuffleAnswers(options)
     }
 }
-
+/*Function to randomize the order of answers displayed. This is based on the Fisher-Yates Shuffle Algorithm
+https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle*/
 function shuffleAnswers(options){
     for( var i = options.length-1; i > 0; i--){
         var tempEnd = options[i]
@@ -43,7 +47,7 @@ function shuffleAnswers(options){
     }
     return options
 }
-
+//Create a function to take the question object and display its possible answers in question box.
 function formateQuestion(questionArray){
     var selectedQuestion = questionArray[Math.floor(Math.random()*questionArray.length)]
     document.querySelector("#question").textContent = selectedQuestion.question;
@@ -58,8 +62,9 @@ function formateQuestion(questionArray){
         }
     }
 }
-
+//Create a function to check if the selected answer is indeed correct or not
 function answerVerification(){
+    //Compare to see if the answer is correct. If so, update the score. Regardless of correctness, style the text.
     if(this.getAttribute('name') == 'correct'){
         var resultText = document.querySelector("#resultText");
         resultText.textContent = "Correct!";
@@ -71,39 +76,49 @@ function answerVerification(){
         resultText.textContent = "Incorrect.";
         resultText.style.color = 'red';
     }
+    //Load the next question
     formateQuestion(questionArray);
 }
-
+/*Create a function to run the game. */
 function runGame(){
+    //Changes screen for the player
     changeBox(questionBox);
+    //Formats and loads the first question
     formateQuestion(questionArray);
+    //Start a timer that counts down by ones
     var timerInterval = setInterval(function() {
     timeVal--;
     timeText.textContent = timeVal;
+    //Listen for player responses, then verify said answer for correctness
     document.getElementById('a1').onclick = answerVerification;
     document.getElementById('a2').onclick = answerVerification;
     document.getElementById('a3').onclick = answerVerification;
     document.getElementById('a4').onclick = answerVerification;
     if(timeVal == 0){
+        //Clear the timer to reset it
         clearInterval(timerInterval);
+        //Display final score
         document.querySelector("#finalScoreVal").textContent = "Final Score: " + score;
+        //Change to the completion screen
         changeBox(quizCompleted)
     }
     }, 1000)
 }
-
+//Create a function to take values entered into the leader form to test if they should be added to the leader board.
 function saveLeader(event){
     event.preventDefault();
     var currentValues = [score, document.getElementById("userName").value]
     processResults(currentValues);
 }
-
+/*Create a function to update the leader board*/
 function processResults(currentValues){
     var leaderArray = []
     var localStorageValues = localStorage.getItem("leaderArray");
+    //Check to see if there is a current stored leader board. If not, just the current values into the array
     if (localStorageValues == null){
         leaderArray.push(currentValues)
     }
+    //If a current leader board exists, process the string data into 2 element arrays, then push them to to the leader array
     else{
         var tempArray = localStorageValues.split(",");
         while(tempArray.length > 0){
@@ -113,6 +128,7 @@ function processResults(currentValues){
         }
         leaderArray.push(currentValues);
     }
+    //If the array has more than 1 value, sort the values by size - largest to smallest
     if (leaderArray.length > 1){
         //This code was researched and copied from: https://stackoverflow.com/questions/50415200/sort-an-array-of-arrays-in-javascript
         leaderArray.sort(function(a, b){
@@ -120,32 +136,36 @@ function processResults(currentValues){
         })
         //Original code begins again here
     }
+    //If there are more than 5 items in the leader array pop the last element, which will be the smallest
     while (leaderArray.length > 5){
         leaderArray.pop()
     }
-
+    //Clear the previous leader board if data exists there
     var leadersOnBoard = document.querySelector("#topScores").children
-
     while (leadersOnBoard.length > 0){
         for (var item of leadersOnBoard){
             item.remove();
         }
     }
+    //Create new elements with the leader board data
     leaderArray.forEach(score =>{
         var scoreItem = document.createElement("li");
         scoreItem.textContent = score[1] + ": " + score[0];
         scoreItem.setAttribute("class", "leaders")
         document.querySelector("#topScores").appendChild(scoreItem)
     })
+    //Update the elements in local storage
     localStorage.setItem("leaderArray", leaderArray)    
+    //Reset variables for next use
     score = 0;
-    timeVal = 10;
+    timeVal = 60;
     timeText.textContent = timeVal;
     document.querySelector("#resultText").textContent = "If you were right or wrong will pop up here";
     document.querySelector("#resultText").style.color = "black";
     changeBox(leaderText)
 }
 
+//Create a function to clear the leader board and its local storage
 function clearBoard(event){
     event.preventDefault();
     localStorage.clear();
@@ -156,8 +176,7 @@ function clearBoard(event){
         }
     }
 }
-
-
+//Use a JSON structure to serve as a question database
 var questionDatabase = {
     "1": [
         "Which of the following will call the 3rd item of an array?",
@@ -195,7 +214,9 @@ var questionDatabase = {
         "None of these are true"
     ]
 }
+//Create variables for reference
 var questionArray = [];
+//Convert the JSON data into question objects and stor them in an array
 prepQuestions(questionArray);
 var leaderText = document.querySelector("#leaderLink");
 var timeText = document.querySelector("#startTime");
@@ -205,6 +226,7 @@ var highScore = document.querySelector("#highScore");
 var quizCompleted = document.querySelector("#quizCompleted");
 var timeVal = document.querySelector("#startTime").innerHTML;
 var score = 0;
+//Add an event listener so the user can gain click-access to the leader board
 leaderText.addEventListener("click", function(event){
     event.preventDefault(event);
     changeBox(leaderText);
